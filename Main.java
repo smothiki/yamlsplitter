5e01a25136ab5a81c1ac904cb51a2268a80fa170
Mothiki <smothiki@lm-sjn-21002821.paypalcorp.com>
2016-11-08 14:56:23 -0800

    split yaml based on delimeter

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.lang.*;
+
+public class Main {
+  public static String delimeter = "#---#";
+  public static void main(String... args) throws Exception {
+    BufferedReader br = new BufferedReader(new FileReader(new File("cars.txt")));
+try{
+    makeCars(br);
+}
+finally {
+                 br.close();
+
+             }
+   }
+
+  public static void makeCars(BufferedReader br) throws Exception {
+
+    String line;
+    while((line = br.readLine()) !=null) {
+      while(line.startsWith(delimeter)) {
+        String name = br.readLine();
+        name = name.substring(1);
+        File file = new File(name);
+        PrintWriter writer = new PrintWriter(file, "UTF-8");
+        String content;
+        while ((content = br.readLine()) !=null){
+          line = content;
+          if (!content.startsWith(delimeter))
+            writer.println(content);
+          else
+            break;
+        }
+        writer.close();
+      }
+    }
+  }
+}
